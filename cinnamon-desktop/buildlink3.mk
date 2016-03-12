5784b1f5f2ad462c15365b49e8efec12e713359b
Ryo ONODERA <ryoon@NetBSD.org>
2016-03-13 08:40:47 +0900

    cinnamon-desktop: Use latest release and regenerate buildlink3.mk

--- cinnamon-desktop/buildlink3.mk
+++ cinnamon-desktop/buildlink3.mk
@@ -1,16 +1,15 @@
 # $NetBSD$
 
-BUILDLINK_TREE+=	cinnamon-menu
+BUILDLINK_TREE+=	cinnamon-desktop
 
-.if !defined(CINNAMON_MENU_BUILDLINK3_MK)
-CINNAMON_MENU_BUILDLINK3_MK:=
+.if !defined(CINNAMON_DESKTOP_BUILDLINK3_MK)
+CINNAMON_DESKTOP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.cinnamon-menu+=	cinnamon-menu>=2.8.0
-BUILDLINK_PKGSRCDIR.cinnamon-menu?=	../../wip-cinnamon/cinnamon-menu
+BUILDLINK_API_DEPENDS.cinnamon-desktop+=	cinnamon-desktop>=2.8.0
+BUILDLINK_PKGSRCDIR.cinnamon-desktop?=	../../wip-cinnamon/cinnamon-desktop
 
-.include "../../devel/gettext-lib/buildlink3.mk"
-.include "../../devel/glib2/buildlink3.mk"
-.include "../../devel/gobject-introspection/buildlink3.mk"
-.endif	# CINNAMON_MENU_BUILDLINK3_MK
+.include "../../x11/gtk3/buildlink3.mk"
+.include "../../x11/libxkbfile/buildlink3.mk"
+.endif	# CINNAMON_DESKTOP_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-cinnamon-menu
+BUILDLINK_TREE+=	-cinnamon-desktop
